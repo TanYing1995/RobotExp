7293f6f57dbd192563436dcbf17c108617dcb0d8
TanYing1995 <694024566@qq.com>
2023-06-10 22:11:10 +0800

    v1.2

--- LSTM/Test.m
+++ LSTM/Test.m
@@ -1,30 +1,50 @@
+% 获取训练数据集中的最值向量
+[input_max,input_min,output_max,output_min] = get_max_min();
 
-data_dir = 'I:\Experiments\LSTM\力矩数据\12';
+dir = 'I:\Experiments\LSTM\力矩数据\2'; 
+% data_dir = 'I:\Experiments\LSTM\数据\2';
 
-input_test = load(fullfile(data_dir, 'input.mat')).input;
-output_test = load(fullfile(data_dir, 'torque.mat')).x;
+input_test = load(fullfile(dir, 'input.mat')).input;
+output_test = load(fullfile(dir, 'torque.mat')).x;
 
 % 将数据分成训练数据和测试数据
 xTrain = input_test;
 tTrain = output_test;
 
+% 输入数据归一化处理
+[nrows, ncols] = size(xTrain);
+    for k=1:nrows
+        row = xTrain(k,:);
+        max_val = input_max(k,1);
+        min_val = input_min(k,1);
+        if min_val == max_val
+            xTrain(k,:) = 0;
+        else
+            xTrain(k,:) = ((row - min_val) / (max_val - min_val))*2-1;
+        end
+    end
+
 tPred = predict(net, xTrain);
 tPred = double(tPred); % 将LSTM网络的输出cell数组转换为矩阵
+
+% % 对数据进行反归一化处理
+for i = 1:6
+    normalized_row = tPred(i,:); % 获取当前行的数据
+    max_val = output_max(i,1); % 获取当前行的最小值
+    min_val = output_min(i,1); % 获取当前行的最大值
+    
+    % 利用反归一化公式，将归一化后的数据还原回原始取值范围
+    original_row = 0.5*(normalized_row+1)*(max_val - min_val) + min_val;
+    tPred(i,:) = original_row; % 将还原后的数据存回矩阵中
+end
 
 rmse = [];
 for i = 1:6
     rmse(i) = sqrt(mean((tTrain(i,:)-tPred(i,:)).^2));
 end
 
-% i = 6;
-% figure(1);
-% plot(tTrain(i,:),'b');
-% % plot(tPred(1,:),'r');
-% xlabel('时间');
-% ylabel('力矩');
-% % hold on;
-% plot(tPred(i,:),'r');
-% legend('实际力矩', '预测力矩');
+%% 输出滤波
+tPred = kalman_filter(tPred);
 
 for i = 1 : 3
    figure(i); 
--- LSTM/train_LSTM.m
+++ LSTM/train_LSTM.m
@@ -9,28 +9,81 @@ num_subdirs = length(all_subdirs);
 numFeatures = 12; % 输入数据特征数量
 numResponses = 6; % 输出数据响应数量
 numHiddenUnits = 128; % LSTM隐藏层神经元数量
-
+% dropoutLayer(0.1)
+%bilstmLayer(2*numHiddenUnits,'OutputMode','sequence')
+%     lstmLayer(numHiddenUnits,'OutputMode','sequence')
 layers = [ ...
     sequenceInputLayer(numFeatures)
-    lstmLayer(2*numHiddenUnits,'OutputMode','sequence')
-    lstmLayer(numHiddenUnits,'OutputMode','sequence')
-    fullyConnectedLayer(256);
-    fullyConnectedLayer(numResponses);
+    gruLayer(2*numHiddenUnits,'OutputMode','sequence')
+    fullyConnectedLayer(128)
+    fullyConnectedLayer(numResponses)
     regressionLayer];
 
 % 定义训练选项和结果评估指标
 options = trainingOptions('adam', ...
-    'MaxEpochs',500, ...
+    'MaxEpochs',300, ...
     'GradientThreshold',1, ...
     'InitialLearnRate',0.01, ...
     'LearnRateSchedule','piecewise', ...
-    'LearnRateDropFactor',0.5, ...
-    'LearnRateDropPeriod',80, ...
-    'Verbose',1, ...
-    'Plots','training-progress');
-
+    'LearnRateDropFactor',0.7, ...
+    'LearnRateDropPeriod',100, ...
+    'Verbose',1);
+% , ...
+%     'Plots','training-progress'
 % 循环遍历每个子目录，读取并预处理输入输出数据，并使用已经创建的LSTM网络进行训练
+
+%% 输入输出归一化
+input_max = -10000000*ones(12,1); %12x1最大值向量
+input_min = 10000000*ones(12,1); %12x1最小值向量
+
+output_max = -10000000*ones(6,1); %6x1最大值向量
+output_min = 10000000*ones(6,1); %6x1最小值向量
+
 for i = 1:num_subdirs  
+    
+    % 获取当前子目录名称
+    subdir_name = all_subdirs(i).name;
+    
+    % 如果当前项不是一个子目录，则忽略
+    if strcmp(subdir_name, '.') || strcmp(subdir_name, '..') || ~isdir(fullfile(data_dir, subdir_name))
+        continue;
+    end
+
+    % 读取输入和输出数据文件
+    input_data_filename = fullfile(data_dir, subdir_name, 'input.mat');
+    output_data_filename = fullfile(data_dir, subdir_name, 'torque.mat');
+    input_data = load(input_data_filename).input;% 12xn输入向量
+    output_data = load(output_data_filename).x; % 6xn输出向量
+    
+    % 输入最大值和最小值
+    for j = 1 : 12
+        row = input_data(j,:);
+        max_val = max(row);
+        min_val = min(row);
+        if max_val > input_max(j,1)
+            input_max(j,1) = max_val;
+        end
+        if min_val < input_min(j,1)
+            input_min(j,1) = min_val;
+        end
+    end
+    
+    % 输出最大值和最小值
+    for j = 1 : 6
+        row = output_data(j,:);
+        max_val = max(row);
+        min_val = min(row);
+        if max_val > output_max(j,1)
+            output_max(j,1) = max_val;
+        end
+        if min_val < output_min(j,1)
+            output_min(j,1) = min_val;
+        end
+    end
+end
+%%
+for i = 1:num_subdirs  
+    
     % 获取当前子目录名称
     subdir_name = all_subdirs(i).name;
     
@@ -48,15 +101,28 @@ for i = 1:num_subdirs
     xTrain = input_data;
     tTrain = output_data; 
     
+    % 训练输入数据归一化 [-1,1]
     [nrows, ncols] = size(xTrain);
     for k=1:nrows
         row = xTrain(k,:);
-        min_val = min(row);
-        max_val = max(row);
+        max_val = input_max(k,1);
+        min_val = input_min(k,1);
         if min_val == max_val
             xTrain(k,:) = 0;
         else
-            xTrain(k,:) = (row - min_val) / (max_val - min_val);
+            xTrain(k,:) = ((row - min_val) / (max_val - min_val))*2-1;
+        end
+    end
+    
+    % 训练输出数据归一化 [-1,1]
+     for k=1:6
+        row = tTrain(k,:);
+        max_val = output_max(k,1);
+        min_val = output_min(k,1);
+        if min_val == max_val
+            tTrain(k,:) = 0;
+        else
+            tTrain(k,:) = ((row - min_val) / (max_val - min_val))*2-1;
         end
     end
     
